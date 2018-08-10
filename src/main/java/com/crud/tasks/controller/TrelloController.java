ee1f24b015d2a48b9c908c93407a9c425798cfb0
marcinzak77 <marcin.zak77@gmail.com>
2018-08-10 15:46:35 +0200

    19.2

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -6,6 +6,7 @@ import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +29,7 @@ public class TrelloController {
 
         return trelloClient.createNewCard(trelloCardDto);
     }
+
 
 
 }
